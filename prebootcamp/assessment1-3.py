dc57e017a779cc98a813f20fd56ec6a890dcb3ef
arian farnia <arian2012f@gmail.com>
2023-12-02 17:20:22 +0330

    a1-3

---
+++ prebootcamp/assessment1-3.py
@@ -0,0 +1,5 @@
+p, d= map(float, input('hey!, pls gimme 1- p num 2- diameter seperated with single space:').split())
+r = float(d/2)
+cp = float(d*p)
+ca = float(r*r*p)
+print('perimeter of circle: %.3f and area of circle: %.3f' %(cp, ca) )
