08783e51fc40b44782bfe017f53e98a1565b8b29
arian farnia <arian2012f@gmail.com>
2023-12-05 18:10:33 +0330

    loop2

--- prebootcamp/assessment3-loops/loop2.py
+++ prebootcamp/assessment3-loops/loop2.py
@@ -5,13 +5,10 @@ while True:
     a = int(input('hey, pls gimme ur numbers:'))
     if a == -1:
         break
-    a = str(a * 2)
-    a = a[::-1]
-    a = int(a)
+    a = int(str(a * 2)[::-1])
     b.append(a)
 
 for i in b:
-    i = int(i)
     c = c + i
 
 print(c)
