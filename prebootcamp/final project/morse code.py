2c2cd1d44d9504b6ff8687af48b217c9b620dc28
arian farnia <arian2012f@gmail.com>
2023-12-10 15:06:21 +0330

    IDK

--- prebootcamp/final project/morse code.py
+++ prebootcamp/final project/morse code.py
@@ -1,13 +1,19 @@
-jomle = input('Hey!, type ur sentence: ')
+jomle = input('Hey!, type ur sentence or morse code: ')
 
 with open('dictionary.py', 'r') as of:
+
     morse_dict = {}
+
     for line in of:
         key, value = line.split('=')
         morse_dict[key.strip()] = value.strip()
-def convert(jx):
+
+
+def convertm(jx):
+
     morse_sen = []
     global morse_dict
+
     for jh in jx: #jh = jomle harf
         if ord(jh) == 32:
             morse_sen.append(morse_dict['space'])
@@ -19,12 +25,52 @@ def convert(jx):
 
     return morse_sen
 
+
+def fkv(dx, valx):
+    kv = ''
+    for key, value in dx.items():
+        if value == valx:
+            kv = kv + key
+    return kv
+
+def convertj(mx):
+
+    global morse_dict
+
+    splited_sen = list(mx.split('     '))
+    splited = ''
+
+    for i in splited_sen:
+        splited_word = i.split(' ')
+
+        for i in splited_word:
+            for j in morse_dict.values():
+                if ord(i) == 32:
+                    continue
+                else:
+                    kv = fkv(morse_dict, j)
+                mkv = mkv + kv
+
+
+
+
+
+
+
+
+
+
+
 for i in jomle:
+
     if i != '.' or i != '-':
-        converted = convert(jomle)
+        converted = convertm(jomle)
 
         print(''.join(converted).replace("'", ""))
-    break
+        break
+
+    else:
+
 
 
 
--- prebootcamp/final project/testbot.py
+++ prebootcamp/final project/testbot.py
@@ -1,6 +1,44 @@
-j = 'fjknfkjv;fjv'
+jomle = input('Hey!, type ur sentence or morse code: ')
 
-for i in j:
-    if i != '.' or i != '-':
-        print(i)
-        break
+with open('dictionary.py', 'r') as of:
+
+    morse_dict = {}
+
+    for line in of:
+        key, value = line.split('=')
+        morse_dict[key.strip()] = value.strip()
+
+
+
+def find_key(dictionary, value):
+    for key, val in dictionary.items():
+        if val == value:
+            return key
+    return None
+
+
+def convertj(mx):
+    mkv = ''
+    jkv = ''
+
+    global morse_dict
+
+    splited_sen = mx.split('     ')
+
+    for word in splited_sen:
+        for char in word.split(' '):
+            for code in morse_dict.values():
+                if char == code:
+                    jkv = find_key(morse_dict, code)
+                    mkv += jkv
+    print(mkv)
+
+
+convertj(jomle)
+
+
+
+
+
+
+
